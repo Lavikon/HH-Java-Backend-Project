be4826428b0e4ab3c1df8221e53f854a9f9c5972
Lavikon <108793040+Lavikon@users.noreply.github.com>
2026-03-31 01:11:55 +0300

    chore: Initial commit

---
+++ src/main/java/hh/backend/littlefreelibrary/LittlefreelibraryApplication.java
@@ -0,0 +1,13 @@
+package hh.backend.littlefreelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittlefreelibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittlefreelibraryApplication.class, args);
+	}
+
+}
