b0ee384f075354a9e1a0cc17c1d16d5eb75d0536
Niko Lavikainen <nikolavikainen@gmail.com>
2026-03-31 13:37:45 +0300

    feat: adds CRUD to LibraryController

--- src/main/java/hh/backend/littlefreelibrary/web/LibraryController.java
+++ src/main/java/hh/backend/littlefreelibrary/web/LibraryController.java
@@ -3,9 +3,12 @@ package hh.backend.littlefreelibrary.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import hh.backend.littlefreelibrary.domain.LibraryRepository;
 import hh.backend.littlefreelibrary.domain.InstitutionRepository;
+import hh.backend.littlefreelibrary.domain.Library;
 import hh.backend.littlefreelibrary.domain.LibraryTypeRepository;
 
 
@@ -25,7 +28,7 @@ public class LibraryController {
 
     // CRUD operations
     // display library list
-    @GetMapping("/list")
+    @GetMapping("/library/list")
     public String getLibraries(Model model) {
         model.addAttribute("libraries", libraryRepository.findAll());
         model.addAttribute("institutions", institutionRepository.findAll());
@@ -34,11 +37,40 @@ public class LibraryController {
     }
     
     // add a library form
+    @GetMapping("/library/new")
+    public String showCreateLibraryForm(Model model) {
+        model.addAttribute("library", new Library());
+        model.addAttribute("institutions", institutionRepository.findAll());
+        model.addAttribute("libraryTypes", libraryTypeRepository.findAll());
+        return "libraryform";
+    }
 
     // Save new library
+    @PostMapping("/library/save")
+    public String saveLibrary(Library library) {
+        libraryRepository.save(library);
+        return "redirect:/library/list";
+    }
 
     // Delete library
+    @GetMapping("/library/delete/{id}")
+    public String deleteLibrary(@PathVariable("id") Integer id) {
+        libraryRepository.deleteById(id);
+        return "redirect:/library/list";
+    }
 
     // Edit library
+    @GetMapping("/library/edit/{id}")
+    public String showEditLibraryForm(@PathVariable("id") Integer id, Model model) {
+        Library library = libraryRepository.findById(id).orElse(null);
+        // If library not found, redirect to list
+        if (library == null) {
+            return "redirect:/library/list";
+        }
+        model.addAttribute("library", library);
+        model.addAttribute("institutions", institutionRepository.findAll());
+        model.addAttribute("libraryTypes", libraryTypeRepository.findAll());
+        return "libraryform";
+    }
 
 }
