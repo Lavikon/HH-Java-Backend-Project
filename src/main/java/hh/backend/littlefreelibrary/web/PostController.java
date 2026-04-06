ef5776cdfdfde8e9c47a442d016349a68d8fea9e
Lavikon <108793040+Lavikon@users.noreply.github.com>
2026-04-06 08:33:10 +0300

    feat: added posting by library ID to library detail view

--- src/main/java/hh/backend/littlefreelibrary/web/PostController.java
+++ src/main/java/hh/backend/littlefreelibrary/web/PostController.java
@@ -3,20 +3,26 @@ package hh.backend.littlefreelibrary.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
+import hh.backend.littlefreelibrary.domain.Library;
+import hh.backend.littlefreelibrary.domain.LibraryRepository;
 import hh.backend.littlefreelibrary.domain.Post;
 import hh.backend.littlefreelibrary.domain.PostRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PostController {
 
     // Database repository
     private final PostRepository postRepository;
+    private final LibraryRepository libraryRepository;  
 
-    public PostController(PostRepository postRepository) {
+    public PostController(PostRepository postRepository, 
+        LibraryRepository libraryRepository) {
         this.postRepository = postRepository;
+        this.libraryRepository = libraryRepository;
     }
 
     // CRUD operations
@@ -27,19 +33,30 @@ public class PostController {
         return "postlist";
         // TODO: create postlist.html and add functionality to it
     }
-    
+
     // add a post form
     @GetMapping("/post/new")
-    public String showCreatePostForm(Model model) {
-        model.addAttribute("post", new Post());
+    public String showCreatePostForm(
+        @RequestParam(required = false) Integer libraryId,
+        Model model) {
+        Post post = new Post();
+        model.addAttribute("post", post);
+        model.addAttribute("libraryId", libraryId);
         return "postform";
         // TODO: create postform.html and add functionality to it
     }
-    
+
     // Save new post
-    @GetMapping("/post/save")
-    public String savePost(Post post) {
+    @PostMapping("/post/save")
+    public String savePost(Post post, @RequestParam(required = false) Integer libraryId) {
+        if (libraryId != null) {
+            Library library = libraryRepository.findById(libraryId).orElse(null);
+            post.setLibrary(library);
+        }
         postRepository.save(post);
+        if (libraryId != null) {
+            return "redirect:/library/" + libraryId;
+        }
         return "redirect:/post/list";
     }
 
