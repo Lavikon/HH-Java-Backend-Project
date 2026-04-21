36bd22e07f6bc49f051da520090315852ec5af67
Lavikon <108793040+Lavikon@users.noreply.github.com>
2026-04-21 06:14:42 +0300

    feat: add index page and controller

--- src/main/java/hh/backend/littlefreelibrary/web/HomeController.java
+++ src/main/java/hh/backend/littlefreelibrary/web/HomeController.java
@@ -1,11 +1,30 @@
 package hh.backend.littlefreelibrary.web;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import hh.backend.littlefreelibrary.domain.Library;
+import hh.backend.littlefreelibrary.domain.LibraryRepository;
+import hh.backend.littlefreelibrary.domain.PostRepository;
 
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
+    // public String home(@RequestParam(required = false) String search, Model model) {
+        // List<Library> libraries;
+        // if (search != null && !search.isBlank()) {
+        //     libraries = LibraryRepository
+        //             .findByNameContainingIgnoreCaseOrLocationContainingIgnoreCaseOrInstitution_NameContainingIgnoreCase(
+        //                     search, search, search);
+        // } else {
+        //     libraries = LibraryRepository.findAll();
+        // }
+        // model.addAttribute("libraries", libraries);
+        // model.addAttribute("search", search);
+        // model.addAttribute("recentPosts", PostRepository.findTop5ByOrderByCreatedAtDesc());
         return "index";
     }
 }
