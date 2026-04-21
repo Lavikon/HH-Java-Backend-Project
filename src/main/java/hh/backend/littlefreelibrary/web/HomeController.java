80ceba3e2d937e4c6485c613fc890f7f746ce723
Lavikon <108793040+Lavikon@users.noreply.github.com>
2026-04-21 06:55:38 +0300

    feat: add library list to homepage

--- src/main/java/hh/backend/littlefreelibrary/web/HomeController.java
+++ src/main/java/hh/backend/littlefreelibrary/web/HomeController.java
@@ -3,20 +3,26 @@ package hh.backend.littlefreelibrary.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import hh.backend.littlefreelibrary.domain.LibraryRepository;
 import hh.backend.littlefreelibrary.domain.PostRepository;
 
 @Controller
 public class HomeController {
 
+    private final LibraryRepository libraryRepository;
     private final PostRepository postRepository;
 
-    public HomeController(PostRepository postRepository) {
+    public HomeController(LibraryRepository libraryRepository, PostRepository postRepository) {
+        this.libraryRepository = libraryRepository;
         this.postRepository = postRepository;
     }
 
     @GetMapping("/")
     //public String home() {
     public String home(Model model) {
+        // librarylist
+        model.addAttribute("libraries", libraryRepository.findAll());
         // Fetch the 5 most recent posts and add to model
         model.addAttribute("recentPosts", postRepository.findTop5ByOrderByCreatedAtDesc());
         return "index";
